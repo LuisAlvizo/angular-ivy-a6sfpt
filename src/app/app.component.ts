562c294b04000ea3305a1848a3f5c9ec0a6acefe
LuisAlvizo <86866325+LuisAlvizo@users.noreply.github.com>
2023-03-24 00:35:10 -0600

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  datos: personas[] = [
+    { id: 1, nombre: 'Luis', apellido: 'Alvizo', sexo: 'M' },
+    { id: 2, nombre: 'Gonzalo', apellido: 'Perez', sexo: 'M' },
+    { id: 3, nombre: 'Monica', apellido: 'Lopez', sexo: 'F' },
+    { id: 4, nombre: 'Juan', apellido: 'Rodriguez', sexo: 'M' },
+    { id: 5, nombre: 'Sofia', apellido: 'Pedroza', sexo: 'F' },
+  ];
+} //fin clase
+
+interface personas {
+  id: number;
+  nombre: string;
+  apellido: string;
+  sexo: string;
 }
